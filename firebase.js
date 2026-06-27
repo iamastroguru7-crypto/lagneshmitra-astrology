280521f90190e44eac4cecde82bef834618d9ebc
iamastroguru7-crypto <iamastroguru7@gmail.com>
2026-06-27 10:06:47 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  // tumhara existing config
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
